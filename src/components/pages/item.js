dd0d46cf6a6d06e082231794b363895c430c4124
Larionova Uliana <ulatlene@mail.ru>
2022-12-11 18:55:46 +0700

    [15883]Получение информации о блюде с сервера
    Вроде реализовала правильно, получает данные по айди блюда.
    Исправления в верстке страницы

--- src/components/pages/item.js
+++ src/components/pages/item.js
@@ -4,16 +4,22 @@ import Header from '../app/Header';
 import Item from "../app/Item";
 import ThemeInvetor from "../app/ThemeInvetor";
 import { Box } from '@mui/material';
+import { useSnackbar } from "notistack";
 
+import _ from '../../../config'
+import fetchData from "../../utils/fetchData";
 
 export default function ({ }) {
     //const [ ratingDish, setRatingDish ] = useState(rating);
-    //const { enqueueSnackbar } = useSnackbar();
-    let id = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
-   /* useEffect(() => {
+    const { enqueueSnackbar } = useSnackbar();
+    let id = "4ee393fc-af18-4636-be23-08dac7a0ede1";
+    let json;
+    const [ children, setChildren ] = useState([]);
+    
+    useEffect(() => {
     (async () => {
-        let json;
-        console.log(currPage);
+        
+        
         try{
         json = await fetchData((new URL(`/api/dish/${id}`, _.api_server)), {}, 'GET');
         }
@@ -22,50 +28,51 @@ export default function ({ }) {
             return false;
         }
         //setLoading(false);
-        console.log(json?.pagination);
-        setPageCount(json?.pagination?.count);
+        console.log(json);
         
-        if (!json.dishes.length) {
+        
+        /*if (!json.dishes.length) {
             setNull(true);
             enqueueSnackbar( 'No dish inform', { variant: 'warning' });
             
             return false;
         }
-        
-        setChildren(json.dishes.map((card) => {
+        */
+        setChildren(((card) => {
 
             return <Item
-                    key={card.id}
-                    id={card.id}
-                    name={card.name}
-                    description={card.description}
-                    price={card.price}
-                    image={card.image}
-                    vegetarian={card.vegetarian}
-                    rating={card.rating}
-                    category={card.category}
+                    key={json.id}
+                    id={json.id}
+                    name={json.name}
+                    description={json.description}
+                    price={json.price}
+                    image={json.image}
+                    vegetarian={json.vegetarian}
+                    rating={json.rating}
+                    category={json.category}
 
                 />;
         }));
     })();
 }, [id]);
 
-*/
+
     return <>
         <Box sx={(theme) => theme.palette.pages.main.Main.bg}>
             <Header />
             <ThemeInvetor>
                 <Box padding={2}>
                     <Center>
-                        <Item key={id}
-                    id={id}
-                    name={"ddsfdf"}
-                    description={"hgsfsdgfjdsagfk agjf ghasdjfhgdsjfg hdf asdfsadjflsf"}
+                        { children
+                        /*<Item key={json.id}
+                    id={json.id}
+                    name={json.name}
+                    description={json.description}
                     price={"1233"}
                     image={"https://funik.ru/wp-content/uploads/2018/10/17478da42271207e1d86.jpg"}
                     vegetarian={"1"}
                     rating={9}
-                    category={"Wok"}/>
+                    category={"Wok"}/>*/}
                     </Center>
                 </Box>
             </ThemeInvetor>
